6665c8ef264129b20bf0c27bc263740d08465055
Daczuk Mutcho Foda <matheus.daczuk.rodrigues@escola.pr.gov.br>
2024-05-10 12:16:34 -0300

    Tipo Null

---
+++ Pasta Trabalho Jose/tipoespecialnull.js
@@ -0,0 +1,11 @@
+// Null
+// Ausencia de Valor
+// Atribuimos valor a uma variavel
+
+const valornulo = null;
+
+// Undefined
+// Ausencia de Valor
+// Mais um tipo diferente de Ausencia de Valor
+
+let valorUndefined;
