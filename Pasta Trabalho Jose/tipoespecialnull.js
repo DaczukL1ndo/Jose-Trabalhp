5b81de120d00a958a7e0925bc3d256de9fbd6355
Daczuk Mutcho Foda <matheus.daczuk.rodrigues@escola.pr.gov.br>
2024-05-10 12:29:07 -0300

    Escrevi algo que esqueci

--- Pasta Trabalho Jose/tipoespecialnull.js
+++ Pasta Trabalho Jose/tipoespecialnull.js
@@ -7,5 +7,7 @@ const valornulo = null;
 // Undefined
 // Ausencia de Valor
 // Mais um tipo diferente de Ausencia de Valor
+// Nao permite uso de const pois variavel 
+// Ser inicializadas
 
 let valorUndefined;
