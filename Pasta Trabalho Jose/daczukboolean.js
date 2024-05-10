0c61a0e43559665082dc38ff77b4fe9e10d02f02
Daczuk Mutcho Foda <matheus.daczuk.rodrigues@escola.pr.gov.br>
2024-05-10 12:09:23 -0300

    aula "2"

---
+++ Pasta Trabalho Jose/daczukboolean.js
@@ -0,0 +1,29 @@
+// Tipo Boolean
+// verdadeiro ou falso
+
+const primeironumero = 5;
+
+//se segundo numero fosse 5, daria true
+const segundonumero =  10; 
+
+// === Compara tipo e valor
+console.log(primeironumero === segundonumero)
+
+// declarar uma variavel com valor boleano
+
+const ativo = false;
+let ativo2 = true; //permite a mudança do valor na variavel
+
+const nome =  "Anna";
+const sobrenome = "Beatriz";
+
+console.log(nome === sobrenome)
+
+
+
+
+// Letra maiuscula e minuscula sao "diferentes"
+const nome1 = "Alura";
+const nome2 = "alura";
+
+console.log (nome1 === nome2)
---
+++ Pasta Trabalho Jose/daczukstring.js
@@ -0,0 +1,24 @@
+// Tipo String
+
+// Conjunto Caracteres: Palavras, Frases, Textos e ETC
+
+const string1 = "Matheus"
+
+const string2 = "Daczuk"
+
+// Concatenar
+// Juntar duas ou mais Strings
+
+const nome = string1 + " " + string2
+
+console.log(nome)
+console.log( )
+
+
+const nome2= "Matheus";
+const sobrenome = "Daczuk";
+const idade = 30;
+
+console.log("nome" = nome2);
+console.log("sobrenome" + sobrenome);
+console.log("idade" = 30)
