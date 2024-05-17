c51228358c2e3cbbf500aee249c04799ab6f5c2f
Matheus Daczuk <matheus.daczuk.rodrigues@escola.pr.gov.br>
2024-05-17 09:57:24 -0300

    Verdadeiro e Falso
    1 ou 0 pipi popo

---
+++ Pasta Trabalho Jose/truthyfalsy.js
@@ -0,0 +1,26 @@
+// truthy e Falsy são tipos primitivos de comparação
+
+// 0 e 1
+
+console.log(0 == false);
+console.log(1 == true);
+console.log(0 ==true);
+console.log(1 == false);
+
+
+console.log("" ==  false);
+
+let minhavar;
+// não recebe nada undefined
+
+let testenull = null;
+
+console.log(minhavar);
+console.log(testenull);
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeoff(minhavar));
+console.log(typeoff(testenull))
+
